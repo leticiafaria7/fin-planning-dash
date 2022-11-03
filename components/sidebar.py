9f11c229cdc53c3239eee25285b98b39271058e8
leticiafaria7 <leticiaemanuella96@gmail.com>
2022-11-02 23:12:42 -0300

    primeiro input de receitas

--- components/sidebar.py
+++ components/sidebar.py
@@ -250,3 +250,35 @@ def toggle_modal(n1, is_open):
 def toggle_modal(n1, is_open):
     if n1:
         return not is_open
+
+@app.callback(
+    Output('store-receitas', 'data'),
+
+    Input('salvar_receita', 'n_clicks'),
+    [
+        State('txt-receita', 'value'),
+        State('valor-receita', 'value'),
+        State('date-receitas', 'date'),
+        State('switches-input-receita', 'value'),
+        State('select_receita', 'value'),
+        State('store-receitas', 'data')
+    ]
+)
+def salve_form_receita(n, descricao, valor, date, switches, categoria, dict_receitas):
+    # import pdb
+    # pdb.set_trace()
+
+    df_receitas = pd.DataFrame(dict_receitas)
+
+    if n and not(valor == '' or valor == None):
+        valor = round(float(valor), 2)
+        date = pd.to_datetime(date).date()
+        categoria = categoria[0]
+        recebido = 1 if 1 in switches else 0
+        fixo = 1 if 2 in switches else 0
+
+        df_receitas.loc[df_receitas.shape[0]] = [valor, recebido, fixo, date, categoria, descricao]
+        df_receitas.to_csv('df_receitas.csv')
+
+    data_return = df_receitas.to_dict()
+    return data_return
